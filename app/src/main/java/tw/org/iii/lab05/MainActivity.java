def0d4d88d305cec2ce2ab1ed801d7a8c5f19ee8
chiache <chiache7410@gmail.com>
2016-11-17 13:39:00 +0800

    2016-11-17_Lab05_clear OK

--- app/src/main/java/tw/org/iii/lab05/MainActivity.java
+++ app/src/main/java/tw/org/iii/lab05/MainActivity.java
@@ -6,18 +6,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button clear;
+    private MyView myView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        clear = (Button)findViewById(R.id.clear);
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        myView = (MyView)findViewById(R.id.myview);
+    }
+    public void clear(View v){
+        myView.clear();
     }
 }
